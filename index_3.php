bb23c49ef264c0bc97644303c7f27d49772bfca7
Justin <jeuteneier@gmail.com>
2014-02-05 11:05:24 -0800

    Initial Commit of Original Files

---
+++ index_3.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="verify-v1" content="kbcIjQVLq9ZjOYXvCH3K3lST2xfqiOIxC8zNjsxG3Lo=" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="description" content="Green Hammer, Design | Build | Energy in Portland, Oregon" />
+<meta name="Keywords" content="green, green building, green builder, portland, oregon, portland oregon, sustainable building, sustainable, natural building, no voc, low voc, environmental, conscious, salvage, salvaged, reclaimed, salvage lumber, salvaged lumber, reclaimed lumber, lumber, wood, home, homes, new home, new homes, remodel, remodeling, green remodel, green remodeling, paint, painting, FSC, Forest Stewardship Council, LEED, construction, general contractor" />
+<meta name="city" content="Portland" />
+<meta name="state" content="Oregon" />
+
+<title>Green Hammer - Design | Build | Energy in Portland, Oregon</title>
+
+<link href="css/main.css" rel="stylesheet" type="text/css" />
+<link href="css/menu.css" rel="stylesheet" type="text/css" />
+
+<script src="scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+<script src="scripts/jquery-1.2.6.min.js" type="text/javascript" ></script>
+<script src="scripts/slideshow.js" type="text/javascript" ></script>
+
+<link rel="icon" href="/justin/green_hammer/images/favicon.ico" type="image/x-icon">
+
+</head>
+
+<body>
+<!--[if lte IE 6]><script src="/ie6/warning.js"></script><script>window.onload=function(){e("/ie6/")}</script><![endif]-->
+
+<!-- Begin Page Wrapper -->
+<div id="wrapper">
+
+<?php include("header.html"); ?>
+
+	<!-- Begin Slideshow -->
+		<!-- this will work with any number of images -->
+		<!-- set the active class on whichever image you want to show up as the default 
+		(otherwise this will be the last image) -->
+
+		<div id="slideshow">
+        
+       	  <div class="active"><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/design1.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/design2.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/design3.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/design4.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/design5.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/design6.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/design7.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/build1.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/build2.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/build3.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/build4.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/build5.jpg" alt="Green building in Portland Oregon" /></a></div>
+                                    
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/build6.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/energy1.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/energy2.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/energy3.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/energy4.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/energy5.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/energy6.jpg" alt="Green building in Portland Oregon" /></a></div>
+                                    
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/energy7.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/energy8.jpg" alt="Green building in Portland Oregon" /></a></div>
+            
+            <div><a href="http://www.greenhammer.com/portfolio.shtml"><img src="slideshow/energy9.jpg" alt="Green building in Portland Oregon" /></a></div>
+    
+		</div>
+	<!-- End Slideshow -->
+
+	  <!-- Begin Content -->
+<div id="content_index_new">
+<div id="mission_solid">Green Hammer’s mission is to massively decrease energy consumption and carbon emissions in our built environment by designing and building ultra high-performance living and work spaces that inspire urban livability. Our passion is to integrate beauty, and affordability with the world’s highest standards for energy performance, while promoting responsible, non-toxic, and local materials. <a href="standards.shtml">Learn more</a></div>
+
+      <div id="announcements">
+      	
+        
+          <h2>Announcements</h2>
+          
+     
+
+ <?php
+   // Include the file that does all the work
+   include("rssannouncement.php");
+   $url = "http://greenhammerconstruction.wordpress.com/category/announcement/feed/"; //this is the blogs url
+   $rss=new rssFeed($url);
+   if($rss->error){
+	  print "<h1>Error:</h1>\n<p><strong>$rss->error</strong></p>";
+   }else{
+
+	  $rss->parse();
+
+	  
+	  $rss->showStories();
+   }
+?>
+
+
+          
+<div id="social_media_new"><a href="http://www.facebook.com/pages/Portland-OR/Green-Hammer-Inc/79162394122" target="_blank"><img src="images/facebook.gif" alt="Facebook" width="113" height="35" class="no_border" /></a> <a href="http://twitter.com/green_hammer" target="_blank"><img src="images/twitter.gif" alt="Twitter" width="117" height="35" class="no_border" /></a></div>
+
+          
+    <!-- AddThis Button BEGIN -->
+<div id="addthis_new">
+<div class="addthis_toolbox_new addthis_default_style">
+<a href="http://www.addthis.com/bookmark.php?v=250&amp;username=xa-4c2c18c44a974c07" class="addthis_button_compact">Share</a>
+<span class="addthis_separator">|</span>
+<a class="addthis_button_facebook"></a>
+<a class="addthis_button_myspace"></a>
+<a class="addthis_button_google"></a>
+<a class="addthis_button_twitter"></a></div>
+</div>
+<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=xa-4c2c18c44a974c07"></script>
+<!-- AddThis Button END -->
+          
+    </div>
+    
+   	<div id="features">
+   	  <div class="feature">
+        	  <h4 class="box_header_top">Green Hammer Publishes &quot;Urbanize &amp; Insulate&quot;</h4>
+        	  <p class="no_margin"><img src="images/urbanize_insulate_thumbnail.jpg" alt="Home Performance Testing" width="90" height="139" class="img_left" />Architectural designer and energy consultant <a href="crew.shtml#dylan">Dylan Lamar</a> has authored his first book, Urbanize &amp; Insulate: The Development of a Fossil-Fuel Exit Strategy. The book provides a succinct and well-researched account of how these two basic agendas can radically reduce carbon-emissions in the built environment. <a href="publications.shtml">Read more</a></p>
+   	  </div>
+              
+              <div class="feature_separator"><img src="images/seperator_horizontal_color.gif" alt="Separator" class="no_border" /></div>
+
+      <div class="feature">
+              <h4 class="box_header_top">America's First Passive House Office Building</h4>
+              <p class="no_margin"><a href="http://www.passivehouse.us/passiveHouse/PHIUSHome.html" target="_blank"><img src="images/PCUN.jpg" alt="Passive House Office Building" width="140" height="69" class="img_left" /></a>Green Hammer is collaborating on the design of America’s first <a href="passive_house.shtml">Passive House</a> office building, the CAPACES Leadership Institute, a project of Oregon’s nonprofit farmworker union, PCUN. Features include a super-insulated shell with triple-pane windows, airtight construction, heat-recovery ventilation, night-flush cooling, a planted roof and shading system, and increased thermal mass (earthen walls). The 2600 square foot office will use 70% less energy than a comparable conventional office building.</p>
+      </div>
+              
+              <div class="feature_separator"><img src="images/seperator_horizontal_color.gif" alt="Separator" class="no_border" /></div>
+            
+      <div class="feature">
+	          <h4 class="box_header_top">LEED Gold home in Estacada featured in Oregonian's Home and Garden Northwest</h4>
+	          <p class="no_margin"><a href="portfolio/nash/portfolio.shtml"><img src="portfolio/nash/thumbnails/nash_web_13.jpg" alt="LEED Gold Home" class="img_left" width="136" height="90" /></a>This <a href="portfolio/nash/portfolio.shtml">home</a> features radiant floor heat, all natural <a href="http://www.durisolbuild.com/thermal-wall-forms.shtml" target="_blank">insulated concrete forms (ICFs)</a>, passive solar design, <a href="http://www.sips.org/content/about/index.cfm?pageId=7" target="_blank">structural insulated roof panels (SIPs)</a>, <a href="http://www.fscus.org/" target="_blank">FSC certified lumber</a>, a <a href="http://en.wikipedia.org/wiki/Bioswale" target="_blank">bioswale  rainwater management system</a>, and  much more! Check out our <a href="portfolio/nash/portfolio.shtml">portfolio</a>.</p>
+	          <p class="no_margin">Read the <a href="http://www.oregonlive.com/hg/index.ssf/2008/11/an_estacada_home_melds_industr.html" target="_blank">article in Home and Garden NW</a>.</p>
+      </div>
+              
+              <div class="feature_separator"><img src="images/seperator_horizontal_color.gif" alt="Separator" class="no_border" /></div>
+            
+      <div class="feature">
+	          <h4 class="box_header_top">Green Hammer provides Home Performance Testing</h4>
+	          <p class="no_margin"><img src="images/thermal image shot.jpg" alt="Home Performance Testing" width="130" height="130" class="img_left" />Green Hammer provides state of the art home  performance testing. Our <a href="http://www.bpi.org/" target="_blank">Building  Performance Institute</a> (BPI) certified technicians utilize their deep  construction knowledge and cutting edge technology to provide you with full  energy performance diagnostics and definitive solutions to save you energy. Read  more about <a href="http://www.bpi.org/standards.aspx">BPI standards</a>.</p>
+      </div>
+      <div style="clear:both"></div>
+   	</div>
+        
+  
+        
+        <div style="clear:both"></div>
+
+                
+  </div>
+    <!-- End Content -->
+    
+    <div id="partners_new">
+        
+		<a href="http://www.northwestenergystar.com/"><img src="images/ENE_NW_v_c.jpg" width="87" height="120" class="partner" alt="Energy Star" /></a>
+		<a href="http://www.bpi.org/"><img src="images/BPI Logo web.jpg" alt="Building Performance Institute" width="107" height="150" class="partner" /></a> 
+		<a href="http://www.fsc.org/"><img src="images/FSC_COC_GH.jpg" alt="Forest Stewardship Council" width="69" height="100" class="partner" /></a> 
+		<a href="http://energytrust.org/trade-ally/"><img src="images/ET Trade allies Color.jpg" alt="Energy Trust" width="140" height="68" class="partner" /></a> 
+		<a href="http://www.usgbc.org/"><img src="images/member_logo_blk.jpg" alt="US Green Building Council" width="72" height="72" class="partner" /></a> 
+		<a href="http://www.usgbc.org/DisplayPage.aspx?CMSPageID=147"><img src="images/leed_homes_logo_orange.jpg" alt="LEED for Homes" width="92" height="136" class="partner" /></a>
+        <a href="http://www.earthadvantage.org/"><img src="images/EAInstitute Logo_color_tag.jpg" alt="Earth Advantage Institute" width="112" height="50" class="partner" /></a>
+	  <div style="clear:both"></div>
+            
+  </div>
+    
+</div>
+<!-- End Page Wrapper -->
+
+<!-- Begin Footer -->
+<?php include("footer.html"); ?>
+<!-- End Footer -->
+<?php include("scripts/ga.js"); ?>
+
+</body>
+</html>
---
+++ rssaward.php
@@ -0,0 +1,355 @@
+<?php
+class rssFeed{
+	var $title="";
+	var $copyright="";
+	var $description="";
+	var $image;
+	var $stories=array();
+	var $url="";
+	var $xml="";
+	var $link="";
+	var $error="";
+	var $maxstories=0;
+	
+	// public methods
+	function parse(){
+		$parser=xml_parser_create();
+		xml_set_element_handler($parser, "startElement", "endElement");
+		xml_set_character_data_handler($parser, "characterData");
+		xml_parse($parser, $this->xml, true)
+			or die(sprintf("XML error: %s at line %d", 
+				xml_error_string(xml_get_error_code($parser)), 
+				xml_get_current_line_number($parser)));
+		xml_parser_free($parser);
+	}
+
+	function showHeading($tag=""){
+		$tag=$tag?$tag:"h1";
+		if($this->title)
+			print "<$tag>$this->title</$tag>\n";
+	}
+
+	function showImage($align=""){
+		$this->image->show($align);
+	}
+
+	function showLink(){
+		if($this->link)
+			print "<a href=\"$this->link\">$this->link</a>\n";
+	}	
+	function showDescription(){
+		if($this->description)
+			print "<p>$this->description</p>\n";
+	}
+
+	function showStories(){
+		echo "<dl>\n";
+		$n=0;
+		foreach($this->stories as $story){
+			$n++;
+			if ($this->maxstories && $n>$this->maxstories)
+				break;
+			$story->show();
+		}
+		echo "</dl>\n";
+	}
+	
+	
+	
+	// Methods used internally
+	// Constructor: Expects one string parameter that is the URI of the RSS feed
+	function rssFeed($uri=''){
+		$this->image=new rssImage();
+		if($uri){
+			$this->url=$uri;
+			$this->getFeed();
+		} else {
+			$this->error="No URL for RSS feed";
+		}
+	}
+	
+	// Retrieves the XML from the RSS supplier
+	function getFeed(){
+		// if we have a URL
+		if ($this->url){
+			if (extension_loaded('curl')) {
+				$this->xml=$this->getRemoteFile($this->url);
+			}
+		}
+	}
+
+	function getRemoteFile($url){
+		$s=new gwSocket();
+		if($s->getUrl($url)){
+			if(is_array($s->headers)){
+				$h=array_change_key_case($s->headers, CASE_LOWER);
+				if($s->error) // failed to connect with host
+					$buffer=$this->errorReturn($s->error);
+				elseif(preg_match("/404/",$h['status'])) // page not found
+					$buffer=$this->errorReturn("Page Not Found");
+				elseif(preg_match("/xml/i",$h['content-type'])) // got XML back
+					$buffer=$s->page;
+				else // got a page, but wrong content type
+					$buffer=$this->errorReturn("The server did not return XML. The content type returned was ".$h['content-type']);
+			} else {
+				$buffer=$this->errorReturn("An unknown error occurred.");
+			}
+		}else{
+			$buffer=$this->errorReturn("An unknown error occurred.");
+		}
+		return $buffer;
+	}
+
+	function errorReturn($error){
+		$retVal="<?xml version=\"1.0\" ?>\n".
+			"<rss version=\"2.0\">\n".
+			"\t<channel>\n".
+			"\t\t<title>Failed to Get RSS Data</title>\n".
+			"\t\t<description>An error was ecnountered attempting to get the RSS data: $error</description>\n".
+			"\t\t<pubdate>".date("D, d F Y H:i:s T")."</pubdate>\n".
+			"\t\t<lastbuilddate>".date("D, d F Y H:i:s T")."</lastbuilddate>\n".
+			"\t</channel>\n".
+			"</rss>\n";
+		return $retVal;
+	}
+
+	function addStory($o){
+		if(is_object($o))
+			$this->stories[]=$o;
+		else
+			$this->error="Type mismatach: expected object";
+	}
+
+}
+
+class rssImage{
+	var $title="";
+	var $url="";
+	var $link="";
+	var $width=0;
+	var $height=0;
+	
+	function show($align=""){
+		if($this->url){
+			if($this->link)
+				print "<a href=\"$this->link\">";
+			print "<img src=\"$this->url\" style=\"border:none;\"";
+			if($this->title)
+				print " alt=\"$this->title\"";
+			if($this->width)
+				print " width=\"$this->width\" height=\"$this->height\"";
+			if($align)
+				print " align=\"$align\"";
+			print ">";	
+			if($this->link)
+				print "</a>";
+		}
+	}
+}
+
+class newsStory{
+	var $title="";
+	var $link="";
+	var $description="";
+	var $pubdate="";
+	
+	function show(){
+		if($this->title){
+			if($this->link){
+				echo "<div class=\"award\"><h4 class=\"no_margin\"><a href=\"$this->link\" target=\"_blank\" class=\"green\">$this->title</a></h4>\n";
+			}elseif($this->title){
+				echo "<div class=\"award\"><h4 class=\"no_margin\">$this->title</a></h4>\n";
+			}
+			echo "<p class=\"no_margin\">";
+			if($this->description)
+				preg_match("/^(\S+\s+){0,37}/", "$this->description", $matches);
+				echo trim($matches[0]) . '...';
+				echo "<a href=\"$this->link\"  target=\"_blank\">Read the full post</a>\n";
+			echo "</p></div>\n";
+		}
+	}
+}
+
+
+class gwSocket{
+	var $Name="gwSocket";
+	var $Version="0.1";
+	var $userAgent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)";
+	var $headers;
+	var $page="";
+	var $result="";
+	var $redirects=0;
+	var $maxRedirects=3;
+	var $error="";
+
+	function getUrl( $url ) {
+		$retVal="";
+		$url_parsed = parse_url($url);
+		$scheme = $url_parsed["scheme"];
+		$host = $url_parsed["host"];
+		$port = $url_parsed["port"]?$url_parsed["port"]:"80";
+		$user = $url_parsed["user"];
+		$pass = $url_parsed["pass"];
+		$path = $url_parsed["path"]?$url_parsed["path"]:"/";
+		$query = $url_parsed["query"];
+		$anchor = $url_parsed["fragment"];
+
+		if (!empty($host)){
+
+			// attempt to open the socket
+			if($fp = fsockopen($host, $port, $errno, $errstr, 2)){
+
+				$path .= $query?"?$query":"";
+				$path .= $anchor?"$anchor":"";
+
+				// this is the request we send to the host
+				$out = "GET $path ".
+					"HTTP/1.0\r\n".
+					"Host: $host\r\n".
+					"Connection: Close\r\n".
+					"User-Agent: $this->userAgent\r\n";
+				if($user)
+					$out .= "Authorization: Basic ".
+						base64_encode("$user:$pass")."\r\n";
+				$out .= "\r\n";
+
+				fputs($fp, $out);
+				while (!feof($fp)) {
+					$retVal.=fgets($fp, 128);
+				}
+				fclose($fp);
+			} else {
+				$this->error="Failed to make connection to host.";//$errstr;
+			}
+			$this->result=$retVal;
+			$this->headers=$this->parseHeaders(trim(substr($retVal,0,strpos($retVal,"\r\n\r\n"))));
+			$this->page=trim(stristr($retVal,"\r\n\r\n"))."\n";
+			if(isset($this->headers['Location'])){
+				$this->redirects++;
+				if($this->redirects<$this->maxRedirects){
+					$location=$this->headers['Location'];
+					$this->headers=array();
+					$this->result="";
+					$this->page="";
+					$this->getUrl($location);
+				}
+			}
+		}
+		return (!$retVal="");
+	}
+	
+	function parseHeaders($s){
+		$h=preg_split("/[\r\n]/",$s);
+		foreach($h as $i){
+			$i=trim($i);
+			if(strstr($i,":")){
+				list($k,$v)=explode(":",$i);
+				$hdr[$k]=substr(stristr($i,":"),2);
+			}else{
+				if(strlen($i)>3)
+					$hdr[]=$i;
+			}
+		}
+		if(isset($hdr[0])){
+			$hdr['Status']=$hdr[0];
+			unset($hdr[0]);
+		}
+		return $hdr;
+	}
+
+}
+
+/*
+	end of classes - global functions follow
+*/
+
+function startElement($parser, $name, $attrs) {
+	global $insideitem, $tag, $isimage;
+	$tag = $name;
+	if($name=="IMAGE")
+		$isimage=true;
+	if ($name == "ITEM") {
+		$insideitem = true;
+	}
+}
+
+function endElement($parser, $name) {
+	global $insideitem, $title, $description, $link, $pubdate, $stories, $rss, $globaldata, $isimage;
+	$globaldata=trim($globaldata);
+	// if we're finishing a news item
+	if ($name == "ITEM") {
+		// create a new news story object
+		$story=new newsStory();
+		// assign the title, link, description and publication date
+		$story->title=trim($title);
+		$story->link=trim($link);
+		$story->description=trim($description);
+		$story->pubdate=trim($pubdate);
+		// add it to our array of stories
+		$rss->addStory($story);
+		// reset our global variables
+		$title = "";
+		$description = "";
+		$link = "";
+		$pubdate = "";
+		$insideitem = false;
+	} else {
+		switch($name){
+			case "TITLE":
+				if(!$isimage)
+					if(!$insideitem)
+						$rss->title=$globaldata;
+				break;
+			case "LINK":
+				if(!$insideitem)
+					$rss->link=$globaldata;
+				break;
+			case "COPYRIGHT":
+				if(!$insideitem)
+					$rss->copyright=$globaldata;
+				break;
+			case "DESCRIPTION":
+				if(!$insideitem)
+					$rss->description=$globaldata;
+				break;
+		}
+	}
+	if($isimage){
+		switch($name){
+			case "TITLE": $rss->image->title=$globaldata;break;
+			case "URL": $rss->image->url=$globaldata;break;
+			case "LINK": $rss->image->link=$globaldata;break;
+			case "WIDTH": $rss->image->width=$globaldata;break;
+			case "HEIGHT": $rss->image->height=$globaldata;break;
+		}
+	}
+	if($name=="IMAGE")	
+		$isimage=false;
+	$globaldata="";
+}
+
+function characterData($parser, $data) {
+	global $insideitem, $tag, $title, $description, $link, $pubdate, $globaldata;
+	if ($insideitem) {
+		switch ($tag) {
+			case "TITLE":
+				$title .= $data;
+				break;
+			case "DESCRIPTION":
+				$description .= $data;
+				break;
+			case "LINK":
+				$link .= $data;
+				break;
+			case "PUBDATE":
+			case "DC:DATE":
+				$pubdate .= $data;
+				break;
+		}
+	} else {
+		$globaldata.=$data;
+	}
+}
+
+?>
+
